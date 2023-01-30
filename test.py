ee07932d216bbcc57b244639d19d07df6ad4e376
Etienne Posthumus <eposthumus@gmail.com>
2023-01-30 14:14:14 +0100

    show files

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-import random, time
+import random, time, os
 
 # AB
 print(time.ctime(), "woot")
 open("docs/zarg.html", "w").write(f"{random.random()}")
+
+for dirpath, dirnames, filenames in os.walk("."):
+    for filename in filenames:
+        print(dirpath, filename)
